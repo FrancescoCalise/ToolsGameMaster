4874d703b6408f6f125046b235299ac65f3b9b77
Francesco Calise <francesco.calise@doit.it>
2024-11-08 17:17:26 +0100

    update dialog

--- src/app/services/dialog.sevice.ts
+++ src/app/services/dialog.sevice.ts
@@ -8,21 +8,22 @@ export class DialogService {
     width: string = '90vw';
     maxWidth: string = '90vw';
     minWidth: string = '90vw'; // Larghezza minima per la dialog
-    minHeight: string = '50%'; // Altezza minima per la dialog
-    maxHeight: string = '80vh'; // Limita l’altezza massima all’80% dell’altezza visibile
-    height: string = '50%'; // Altezza della dialog
+    minHeight: string = '90vh'; // Altezza minima per la dialog
+    maxHeight: string = '90vh'; // Limita l’altezza massima all’80% dell’altezza visibile
+    height: string = '90vh'; // Altezza della dialog
     panelClass: string = '';
 
     constructor(private matDialog: MatDialog) { }
 
     public open(component: any, config: MatDialogConfig = {}): MatDialogRef<any> {
         let isMobile = window.innerWidth < 768;
-        this.minHeight = isMobile ? '80%' : '50%';
+        this.minHeight = isMobile ? '90vh' : '90vh';
         
         let dialogConfig = {};
         if (config.panelClass) {
             dialogConfig = {
                 panelClass: config.panelClass,
+                disableClose: true,
                 ...config
             }
         } else {
@@ -33,6 +34,7 @@ export class DialogService {
                 maxHeight: config.maxHeight ? config.maxHeight : this.maxHeight,
                 minHeight: config.minHeight ? config.minHeight : this.minHeight,
                 height: config.height ? config.height : this.height,
+                disableClose: true,
                 ...config
             };
         }
